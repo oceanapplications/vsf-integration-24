1230960a8bd60cd9a1b8bc8df108ec2d68eee7fe
Sean Baker <oceanapplications@gmail.com>
2024-03-02 18:20:20 -0700

    Added types

--- packages/api-client/src/api/getProduct/index.ts
+++ packages/api-client/src/api/getProduct/index.ts
@@ -1,4 +1,4 @@
-import { Endpoints, ID, TODO } from '../../types';
+import { Endpoints, ID, TODO, SfImage, SfProduct } from '../../types';
 
 export const getProduct: Endpoints['getProduct'] = async (
   context,
@@ -6,50 +6,50 @@ export const getProduct: Endpoints['getProduct'] = async (
 ) => {
   console.log('getProduct has been called');
   const data = await context.client.get('products/' + params.id);
-
-  return {
-      id: data.data.data.id,
-      sku: data.data.data.sku,
-      name: data.data.data.name,
-      slug: data.data.data.name.replace(/\s+/g, '-').toLowerCase() + '-' + data.data.data.id,
-      description: data.data.data.description,
-      price: {
-        isDiscounted: false,
-        regularPrice: {
-          currency: "USD",
-          amount: data.data.data.price,
-          precision: 2
-        },
-        value: {
-          currency: "USD",
-          amount: data.data.data.price,
-          precision: 2
-        },
+  let output: SfProduct = {
+    id: data.data.data.id,
+    sku: data.data.data.sku,
+    name: data.data.data.name,
+    slug: data.data.data.name.replace(/\s+/g, '-').toLowerCase() + '-' + data.data.data.id,
+    description: data.data.data.description,
+    price: {
+      isDiscounted: false,
+      regularPrice: {
+        currency: "USD",
+        amount: data.data.data.price,
+        precisionAmount: "2"
       },
-      primaryImage: getPrimaryImage(data.data.data),
-      gallery: getGallery(data.data.data),
-      rating: {
-        average: 5,
-        count: 99,
+      value: {
+        currency: "USD",
+        amount: data.data.data.price,
+        precisionAmount: "2"
       },
-      //variants: SfProductVariant[];
-      //attributes: SfAttribute[];
-      quantityLimit: null,
-    };
+    },
+    primaryImage: getPrimaryImage(data.data.data),
+    gallery: getGallery(data.data.data),
+    rating: {
+      average: 5,
+      count: 99,
+    },
+    variants: [],
+    attributes: [],
+    quantityLimit: null,
+  };
+  return output;
 };
 
-function getPrimaryImage(data) {
+function getPrimaryImage(data): SfImage {
   return {
-    image: data.gallery[0]?.url ?? null,
+    url: data.gallery[0]?.url ?? null,
     alt:  data.gallery[0]?.alt ?? null,
   };
 }
 
-function getGallery(data) {
+function getGallery(data): SfImage[] {
   let result = [];
   data.gallery.forEach(d => {
     result.push({
-      image: d.url,
+      url: d.url,
       alt: d?.alt ?? null,
     })
   })
